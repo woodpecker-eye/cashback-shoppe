2b05c97abfdd09c2c3e541e8eebdb345346d7e32
Woodpecker <>
2025-12-27 17:33:50 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [
+        {
+            name: 'shoback',
+            script: 'bin/www',
+            exec_mode: 'fork',
+            instances: 1,
+            restart_delay: 10000,
+            autorestart: true,
+            watch: false,
+            time: true,
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: 'root',
+            host: '209.97.175.140',
+            ref: 'origin/main',
+            repo: 'git@github.com:woodpecker-eye/cashback-shoppe.git',
+            ssh_options: ['ForwardAgent=yes', 'PasswordAuthentication=no', 'StrictHostKeyChecking=no'],
+            path: '/opt/shoback',
+            "post-deploy": "npm install"
+        },
+    }
+};
